c3622f10ce458e39d248e48dc6ea33aa0d7e0f3b
Raymon Mens <r.mens@me.com>
2022-12-20 00:06:15 +0100

    Stream on maximal quality in mp3

--- install.sh
+++ install.sh
@@ -110,7 +110,7 @@ fi
 
 # Set the ffmpeg variables based on the value of OUTPUT_FORMAT
 if [ "$OUTPUT_FORMAT" = "mp3" ]; then
-  FF_AUDIO_CODEC='libmp3lame -qscale:a 0'
+  FF_AUDIO_CODEC='libmp3lame -b:a 320k'
   FF_CONTENT_TYPE='audio/mpeg'
   FF_OUTPUT_FORMAT='mp3'
 elif [ "$OUTPUT_FORMAT" = "ogg/vorbis" ]; then
