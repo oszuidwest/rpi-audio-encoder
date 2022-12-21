64b6e05f61ce4ce66e75d12f08aabf435b7c2c22
Raymon Mens <r.mens@me.com>
2022-12-21 13:36:29 +0100

    Add some validation

--- silencedetector.sh
+++ silencedetector.sh
@@ -4,11 +4,21 @@
 THRESHOLD=-40
 DURATION=5
 
-# Record audio from the default microphone and calculate the RMS power using ffmpeg
-ffmpeg -hide_banner -f alsa -channels 2 -sample_rate 48000 -hide_banner -i default:CARD=sndrpihifiberry -t "$DURATION" -f wav -af "volumedetect" -f null /dev/null 2>&1 | \
+# Record audio from the default input and output volume
+mean_volume=$(ffmpeg -hide_banner -f alsa -channels 2 -sample_rate 48000 -hide_banner -i default:CARD=sndrpihifiberry -t "$DURATION" -f wav -af "volumedetect" -f null /dev/null 2>&1 | \
 
-# Extract the RMS power value from the output of ffmpeg
-grep -oP '(?<=mean_volume: ).*(?= dB)' | \
+# Extract the mean_volume value from the output of ffmpeg
+grep -oP '(?<=mean_volume: ).*(?= dB)')
 
-# Compare the mean_volume to the threshold to determine if it is silent
-awk -v threshold="$THRESHOLD" '{if ($1 < threshold) print "silent"; else print "not silent"}'
+# Verify that the mean_volume extraction works. It should be a number that starts with the "-" sign
+if [[ $mean_volume =~ ^-([0-9]+) ]]; then
+  # Compare the mean_volume to the threshold to determine if it is silent
+  if [[ $mean_volume -lt $THRESHOLD ]]; then
+    echo "silent"
+  else
+    echo "not silent"
+  fi
+else
+  # If the regular expression does not match, bail
+  echo "Invalid mean_volume value: $mean_volume"
+fi
