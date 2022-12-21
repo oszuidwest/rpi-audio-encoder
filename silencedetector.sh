1b665b1f7154d1b7e4b2a0b99b2efab1f440c7ac
Raymon Mens <r.mens@me.com>
2022-12-21 13:27:20 +0100

    Fix and simplify the silence detector

--- silencedetector.sh
+++ silencedetector.sh
@@ -8,10 +8,7 @@ DURATION=5
 ffmpeg -hide_banner -f alsa -channels 2 -sample_rate 48000 -hide_banner -i default:CARD=sndrpihifiberry -t "$DURATION" -f wav -af "volumedetect" -f null /dev/null 2>&1 | \
 
 # Extract the RMS power value from the output of ffmpeg
-grep "mean_volume" | \
+grep -oP '(?<=mean_volume: ).*(?= dB)' | \
 
-# Extract the RMS power value and convert it to dB
-awk '{print $4}' | awk '{print 20*log($1)/log(10)}' | \
-
-# Compare the RMS power value to the threshold to determine if it is silent
+# Compare the mean_volume to the threshold to determine if it is silent
 awk -v threshold="$THRESHOLD" '{if ($1 < threshold) print "silent"; else print "not silent"}'
