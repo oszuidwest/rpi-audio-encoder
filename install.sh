0047f35cc67a89c85156f486613a7e025c9490ca
Raymon Mens <r.mens@me.com>
2023-06-27 20:21:11 +0200

    fix this

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ clear
 
 # Download the functions library
 if ! curl -s -o /tmp/functions.sh https://raw.githubusercontent.com/oszuidwest/bash-functions/main/common-functions.sh; then
-  echo -e  *** Failed to download functions library. Please check your network connection! ***"
+  echo -e  "*** Failed to download functions library. Please check your network connection! ***"
   exit 1
 fi
 
