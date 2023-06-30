eccf0ce6b7e651edfb38fc9bac481978ef0a2b51
rmens <r.mens@me.com>
2023-06-30 21:51:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -179,6 +179,6 @@ fi
 
 # Fin 
 echo -e "\n${GREEN}✓ Success!${NC}"
-echo -e "Reboot this device and streaming to ${BOLD}$STREAM_HOST${NC} should start!"
+echo -e "Reboot this device and streaming to ${BOLD}$STREAM_HOST${NC} should start."
 echo -e "You can connect to it's IP in the brower on port ${BOLD}$WEB_PORT${NC}."
 echo -e "The user is ${BOLD}$WEB_USER${NC} and the password you choose is ${BOLD}$WEB_PASSWORD${NC}.\n"
